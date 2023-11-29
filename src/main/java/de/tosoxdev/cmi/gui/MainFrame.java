dff088b62261e455312922bd53cc72b285c513ca
TosoxDev <57193602+TosoxDev@users.noreply.github.com>
2023-11-29 22:57:36 +0100

    docs: add Javadoc comments

--- src/main/java/de/tosoxdev/cmi/gui/MainFrame.java
+++ src/main/java/de/tosoxdev/cmi/gui/MainFrame.java
@@ -5,7 +5,19 @@ import de.tosoxdev.cmi.gui.utils.GridBagConstraintsBuilder;
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ * The {@code MainFrame} class represents the main application window.
+ */
 public class MainFrame extends JFrame {
+    /**
+     * Constructs a new {@code MainFrame} with the specified arguments.
+     * Initializes components and sets default window properties.
+     *
+     * @param title The title of the main frame
+     * @param width The width of the main frame
+     * @param height The height of the main frame
+     * @param opacity The opacity of the main frame (0.0f to 1.0f)
+     */
     public MainFrame(String title, int width, int height, float opacity) {
         initializeComponents();
 
@@ -16,12 +28,16 @@ public class MainFrame extends JFrame {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
-    public void setVisible() {
+    /**
+     * Makes the main window visible.
+     */
+    public void showWindow() {
         this.setVisible(true);
     }
 
     /**
-     * This overrides the {@code java.awt.Frame.isUndecorated} method so the opacity can be set without un-decorating the window
+     * This overrides the {@link java.awt.Frame#isUndecorated()} method
+     * so the opacity can be set without un-decorating the window.
      *
      * @return true
      */
@@ -30,6 +46,9 @@ public class MainFrame extends JFrame {
         return true;
     }
 
+    /**
+     * Initializes the main components of the main frame.
+     */
     private void initializeComponents() {
         JPanel pnlMain = new JPanel(new GridBagLayout());
         {
