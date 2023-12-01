30f95cc4c15ad9152d09fc2366eb9b9cd584049a
TosoxDev <57193602+TosoxDev@users.noreply.github.com>
2023-12-01 12:23:54 +0100

    feat: add CrashHandler and Localizer to Main class

--- src/main/java/de/tosoxdev/cmi/Main.java
+++ src/main/java/de/tosoxdev/cmi/Main.java
@@ -2,13 +2,26 @@ package de.tosoxdev.cmi;
 
 import com.formdev.flatlaf.themes.FlatMacDarkLaf;
 import de.tosoxdev.cmi.gui.MainFrame;
+import de.tosoxdev.cmi.handler.CrashHandler;
+import de.tosoxdev.cmi.localizer.Localizer;
 import de.tosoxdev.cmi.utils.Globals;
 
 public class Main {
+    private static final CrashHandler CRASH_HANDLER = new CrashHandler();
+    private static final Localizer LOCALIZER = new Localizer(Localizer.Language.EN_US);
+
     public static void main(String[] args) {
         FlatMacDarkLaf.setup();
 
         MainFrame mainFrame = new MainFrame(Globals.APP_NAME, 800, 600, 0.9f);
         mainFrame.showWindow();
+    }
+
+    public static CrashHandler getCrashHandler() {
+        return CRASH_HANDLER;
+    }
+
+    public static Localizer getLocalizer() {
+        return LOCALIZER;
     }
 }
