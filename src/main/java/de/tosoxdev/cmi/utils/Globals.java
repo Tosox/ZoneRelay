f837539c2d91ed69a3195fd41b30772f0e202a44
TosoxDev <57193602+TosoxDev@users.noreply.github.com>
2023-12-04 21:15:36 +0100

    feat: change project name

--- src/main/java/de/tosoxdev/cmi/utils/Globals.java
+++ src/main/java/de/tosoxdev/cmi/utils/Globals.java
@@ -2,7 +2,8 @@ package de.tosoxdev.cmi.utils;
 
 public class Globals {
     public static final String APP_VERSION = "0.1.0";
-    public static final String APP_NAME = "S.T.A.L.K.E.R. Custom Modlist Installer";
+    public static final String APP_NAME = "Automated Modlist Installer";
 
     public static final String DIR_LOCALES = "./locales";
+    public static final String DIR_LOGS = "./logs";
 }
