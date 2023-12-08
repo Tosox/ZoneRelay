3f96c1f9ec9532d694c8bd02fdb7940427b1c865
TosoxDev <57193602+TosoxDev@users.noreply.github.com>
2023-12-08 13:17:11 +0100

    feat: add test print

--- src/main/java/de/tosoxdev/ami/gui/controllers/MainFrameController.java
+++ src/main/java/de/tosoxdev/ami/gui/controllers/MainFrameController.java
@@ -1,8 +1,10 @@
 package de.tosoxdev.ami.gui.controllers;
 
+import de.tosoxdev.ami.Main;
+
 public class MainFrameController {
     public void onInstallClick() {
-
+        Main.getLogger().info("Logger");
     }
 
     public void onLaunchClick() {
